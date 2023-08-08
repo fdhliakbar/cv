e2601986c14c988114ebef71bf3e72f28614edff
Soryu <104522615+fdhliakbar@users.noreply.github.com>
2023-08-08 22:01:38 +0700

    Create an isSorting function

--- fix.js
+++ fix.js
@@ -1,2 +1,12 @@
-let lonely = "you look lonely. i can fix that";
-var hai = 10;
+function isSorting(let numbers) {
+  for (let i = 0; i < numbers - 1; i++){
+    for (let j = 0; i < numbers; j++){
+      if (numbers[i] < numbers[j]) {
+        let temp = numbers[i]
+        numbers[i] = numbers[j+1];
+        numbers[j+1] = temp;
+      }
+    }
+  }
+}
+console.log(isSorting(numbers));
